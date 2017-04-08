027f8bd39e69938d2845efc17a7337f0813c8c9e
Elan Baharie <0elan1@gmail.com>
2017-04-08 11:22:35 -0800

    make a page with a background img and a css dir and js dir

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//U.I.
+
+var first = function(arguement) {
+  document.getElementById("trns-form").reset();
+
+
+};
+
+
+//B.L.
+
+$(document).ready(function() {
+  $("#trns-form").submit(function(event) {
+    event.preventDefault();
+    var formSent = $("#userSent").val();
+    var ciphRes = first(formSent);
+
+    $("#response").val(ciphRes);
+  });
+
+});// Doc Ready
